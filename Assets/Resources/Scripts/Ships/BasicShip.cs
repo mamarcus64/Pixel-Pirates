66013701adf40dea6488a6fd9d2a0594df084a7c
Marcus Ma <32466398+Bedwardism@users.noreply.github.com>
2019-10-31 19:33:05 -0700

    lalahoohoo

--- Assets/Resources/Scripts/Ships/BasicShip.cs
+++ Assets/Resources/Scripts/Ships/BasicShip.cs
@@ -9,22 +9,18 @@ public class BasicShip : Ship {
 		spritePath = "Sprites/Ships/basic ship";
 		width = 1280 / 100;
 		height = 432 / 100;
-        //the way roomPositions currently works: the first two values are the x and y position relative to the center of the ship
-        //the third value is essentially an enum that corresponds to the type of room; 0 = 1x1 (single) room, 1 = 2x1 (long) room, 
-        //2 = 1x2 (tall) room and 3 = 2x2 (big) room; to be honest, because rooms are not going to change from copies of the same ship,
-        //the room positions can just be manually entered in code every time for however many types of ships we will design
         /*
-        roomPositions.Add(new Vector3(-1, -0.75f, 3));
-        roomPositions.Add(new Vector3(-1, 0.75f, 3));
-        roomPositions.Add(new Vector3(-1, 0, 1));
-        roomPositions.Add(new Vector3(0, 0, 1));
-        roomPositions.Add(new Vector3(-0.5f, -0.75f, 2));
-        roomPositions.Add(new Vector3(-0.5f, 0.75f, 2));
-        roomPositions.Add(new Vector3(0.5f, -0.75f, 2));
-        roomPositions.Add(new Vector3(0.5f, 0.75f, 2));
-        roomPositions.Add(new Vector3(-2, -0.75f, 3));
-        roomPositions.Add(new Vector3(-2.5f, 0, 1));
+         the way roomPositions currently works: the first two values are the x and y position relative to the center of the ship
+        the third value is essentially an enum that corresponds to the type of room; 0 = 1x1 (single) room, 1 = 2x1 (long) room, 
+        2 = 1x2 (tall) room and 3 = 2x2 (big) room; to be honest, because rooms are not going to change from copies of the same ship,
+        the room positions can just be manually entered in code every time for however many types of ships we will design
+
+        With that being said, the way rooms are generated are with and x and y corresponding to the relative location of the topleft most cell in the room
+        i.e. (0, 0, 2) would be a tall room with the top cell of the room (since there is no topleft cell) located at (0, 0) relative to the rest of the cells
+        and (1, 0, 2) would be a tall room directly to the right of it, since the x value is one greater
+        add an offset after ShipStart to move EVERY room in that direction
         */
+        
         roomPositions.Add(new Vector3(-1, 0, 1));
         roomPositions.Add(new Vector3(-1, 1, 1));
         roomPositions.Add(new Vector3(-1, 3, 2));
